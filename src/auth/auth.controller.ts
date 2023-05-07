4fa70a7c67f7b02b98c9563477659d63aa3766d8
Jean Carlo Urrego <jctaziturno@gmail.com>
2023-05-07 12:16:55 -0500

    Custom Decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Req } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './decorators/get-user.decorator';
+import { User } from './entities';
 
 @Controller('auth')
 export class AuthController {
@@ -20,10 +22,14 @@ export class AuthController {
 
   @Get('private')
   @UseGuards( AuthGuard() )
-  testingPrivateRoute(){
+  testingPrivateRoute(
+    @GetUser() user: User, //Método Personalizado 
+    //@Req() request: Express.Request //MeTodo Manual
+  ){    
     return{
       ok: true,
-      message: 'Hola mundo'
+      message: 'Hola mundo',
+      user: user
     }
   }
 }
